86e65b7132fb7fa1c0b46bcc0fecfde21131040b
Adil Moumni Abdou <moumniabdou.adil@gmail.com>
2022-08-26 11:01:44 +0100

    update

--- dist/index.js
+++ dist/index.js
@@ -3738,6 +3738,122 @@ module.exports = {
 
 /***/ }),
 
+/***/ 2437:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const fs = __nccwpck_require__(7147)
+const path = __nccwpck_require__(1017)
+const os = __nccwpck_require__(2037)
+
+const LINE = /(?:^|^)\s*(?:export\s+)?([\w.-]+)(?:\s*=\s*?|:\s+?)(\s*'(?:\\'|[^'])*'|\s*"(?:\\"|[^"])*"|\s*`(?:\\`|[^`])*`|[^#\r\n]+)?\s*(?:#.*)?(?:$|$)/mg
+
+// Parser src into an Object
+function parse (src) {
+  const obj = {}
+
+  // Convert buffer to string
+  let lines = src.toString()
+
+  // Convert line breaks to same format
+  lines = lines.replace(/\r\n?/mg, '\n')
+
+  let match
+  while ((match = LINE.exec(lines)) != null) {
+    const key = match[1]
+
+    // Default undefined or null to empty string
+    let value = (match[2] || '')
+
+    // Remove whitespace
+    value = value.trim()
+
+    // Check if double quoted
+    const maybeQuote = value[0]
+
+    // Remove surrounding quotes
+    value = value.replace(/^(['"`])([\s\S]*)\1$/mg, '$2')
+
+    // Expand newlines if double quoted
+    if (maybeQuote === '"') {
+      value = value.replace(/\\n/g, '\n')
+      value = value.replace(/\\r/g, '\r')
+    }
+
+    // Add to object
+    obj[key] = value
+  }
+
+  return obj
+}
+
+function _log (message) {
+  console.log(`[dotenv][DEBUG] ${message}`)
+}
+
+function _resolveHome (envPath) {
+  return envPath[0] === '~' ? path.join(os.homedir(), envPath.slice(1)) : envPath
+}
+
+// Populates process.env from .env file
+function config (options) {
+  let dotenvPath = path.resolve(process.cwd(), '.env')
+  let encoding = 'utf8'
+  const debug = Boolean(options && options.debug)
+  const override = Boolean(options && options.override)
+
+  if (options) {
+    if (options.path != null) {
+      dotenvPath = _resolveHome(options.path)
+    }
+    if (options.encoding != null) {
+      encoding = options.encoding
+    }
+  }
+
+  try {
+    // Specifying an encoding returns a string instead of a buffer
+    const parsed = DotenvModule.parse(fs.readFileSync(dotenvPath, { encoding }))
+
+    Object.keys(parsed).forEach(function (key) {
+      if (!Object.prototype.hasOwnProperty.call(process.env, key)) {
+        process.env[key] = parsed[key]
+      } else {
+        if (override === true) {
+          process.env[key] = parsed[key]
+        }
+
+        if (debug) {
+          if (override === true) {
+            _log(`"${key}" is already defined in \`process.env\` and WAS overwritten`)
+          } else {
+            _log(`"${key}" is already defined in \`process.env\` and was NOT overwritten`)
+          }
+        }
+      }
+    })
+
+    return { parsed }
+  } catch (e) {
+    if (debug) {
+      _log(`Failed to load ${dotenvPath} ${e.message}`)
+    }
+
+    return { error: e }
+  }
+}
+
+const DotenvModule = {
+  config,
+  parse
+}
+
+module.exports.config = DotenvModule.config
+module.exports.parse = DotenvModule.parse
+module.exports = DotenvModule
+
+
+/***/ }),
+
 /***/ 1133:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
@@ -4816,7 +4932,8 @@ const axios = __nccwpck_require__(6545)
 const core = __nccwpck_require__(2186)
 const { exec } = __nccwpck_require__(2081);
 const uuid4 = __nccwpck_require__(1868);
-
+const dotenv = __nccwpck_require__(2437);
+dotenv.config();
 // get parameter url from action input
 const APPLICATION_URL = strip(process.env.INPUT_APPLICATION_URL);
 
@@ -4836,36 +4953,39 @@ const WAITING_EXECUTION_TIME = parseInt(
 // Keep track of all jobs
 const jobsStatus = [];
 
-const docker = `
-version: "3.1"
-services:
-  testproject-agent:
-    image: testproject/agent:latest
-    container_name: testproject-agent
-    depends_on:
-      - chrome
-    environment:
-      TP_API_KEY: "${strip(process.env.INPUT_API_KEY)}"
-      TP_AGENT_ALIAS: "Agent auto generater test"
-      TP_AGENT_TEMP: "true"
-      TP_SDK_PORT: "8686"
-      CHROME: "chrome:4444"
-      CHROME_EXT: "localhost:5555"
-    ports:
-    - "8585:8585"
-  chrome:
-    image: selenium/standalone-chrome
-    volumes:
-      - /dev/shm:/dev/shm
-    ports:
-    - "5555:4444`;
+// const docker = `
+// version: "3.1"
+// services:
+//   testproject-agent:
+//     image: testproject/agent:latest
+//     container_name: testproject-agent
+//     depends_on:
+//       - chrome
+//     environment:
+//       TP_API_KEY: "${TP_API_KEY}"
+//       TP_AGENT_ALIAS: "Agent auto generater"
+//       TP_AGENT_TEMP: "true"
+//       TP_SDK_PORT: "8686"
+//       CHROME: "chrome:4444"
+//       CHROME_EXT: "localhost:5555"
+//     ports:
+//     - "8585:8585"
+//   chrome:
+//     image: selenium/standalone-chrome
+//     volumes:
+//       - /dev/shm:/dev/shm
+//     ports:
+//     - "5555:4444"`;
 
 async function runAgent(uuidAgent) {
   try {
     core.info("Create agent");
     core.info("Run cmd export variable and run agent docker ");
     let { stdout } = await sh(`
-    envsubst < ${docker} > docker-compose.yml
+    export TP_API_KEY=${strip(process.env.INPUT_API_KEY)}
+    export TP_AGENT_ALIAS=${uuidAgent}
+    echo $TP_AGENT_ALIAS
+    envsubst < ./docker-compose.yml > docker-compose.yml
     cat docker-compose.yml
     docker-compose -f docker-compose.yml up -d
    `);
